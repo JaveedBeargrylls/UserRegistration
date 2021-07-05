21419e701ebcf854d63a28a25e8c521dcaa1724e
JaveedBeargrylls <76989549+JaveedBeargrylls@users.noreply.github.com>
2021-07-05 22:01:49 +0530

    Modified to the Validation of LastName During the User-Entry

--- UserRegistration/src/com/bridgelabz/userRegistration/UserRegistration.java
+++ UserRegistration/src/com/bridgelabz/userRegistration/UserRegistration.java
@@ -7,26 +7,45 @@ class  Verify
 {
 	Scanner sc = new Scanner(System.in);
 	
-	public String userName;
+	public String firstName;
+	public String lastName;
 	private String userNameRegex;
 	
 	public void userName()
 	{
 		userNameRegex = "[A-Z][a-z]{2,}";
 		
+		// Validation of First Name
+		
 		System.out.println("Enter First Name of User \nStart with UpperCase ");
-		userName = sc.next();
+		firstName = sc.next();
+		
+		// Validation of Last Name
+		
+		System.out.println("Enter Last Name of User \nStart with UpperCase ");
+		lastName = sc.next();
 		
 		Pattern pt = Pattern.compile(userNameRegex);
 		
-		if(userName.matches(userNameRegex))
+		if(firstName.matches(userNameRegex) && lastName.matches(userNameRegex))
 		{
 			System.out.println("Name is Valid");
+			
+			System.out.println("UserName = "+firstName+" "+lastName);
 		}
 		else
 		{
-			System.out.println("Name is not Valid \nRe-Enter Valid name");
+			System.out.println("Name is not Valid"); //\nRe-Enter Valid name\n Check the First name or Last name ");
+			if(firstName.matches(userNameRegex))
+			{
+				System.out.println("You Entered the Invalid LastName");
+			}
+			else
+			{
+				System.out.println("You Entered the Invalid FirstName");
+			}
 		}
+		
 	}
 }
 
